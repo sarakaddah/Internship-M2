dc0fceed39d97baf9938285768757f4d79649efe
sarakaddah <sara.kaddah98@gmail.com>
2021-09-19 11:08:56 +0200

    Heatmap training evolution

--- gym-CartPole_Continuous/gym_CartPole_Continuous/envs/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from gym_CartPole_Continuous.envs.CartPole_Continuous import CartPole_Continuous
--- gym-CartPole_Continuous/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from setuptools import setup
-
-setup(name='gym_CartPole_Continuous',
-      version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
-)
